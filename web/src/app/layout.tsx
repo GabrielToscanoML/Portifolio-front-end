cca7e76dd3022f4afdd55fd5c3b4719c53458b29
Gabriel <gabrieltoscano1@outlook.com>
2024-04-01 11:35:16 -0300

    Arrumando o aninhamento de algumas tags e adicionando um favicon

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`${roboto.variable} ${kanit.variable} font-sans`}>{children}</body>
     </html>
   )
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -10,13 +10,13 @@ export function Header() {
                 <section className="flex flex-col justify-evenly max-w-screen-sm p-4 lg:max-w-lg xl:max-w-xl">
                     <h1 className="text-3xl font-alt mb-4 xl:text-5xl">Olá! Sou <span className="text-[#666BF6]">Toscano</span>, Desenvolvedor Web</h1>
                     <p className="text-lg font-normal xl:text-xl">Me chamo Gabriel Toscano, sou de Goiânia - GO. Desenvolvedor Full-Stack, entusiasta no mundo de desenvolvimento, apaixonado por jogos, músicas. <br /> Estou sempre em busca de novas oportunidades para aperfeiçoar minhas habilidades e trabalhar em projetos desafiadores.</p>
-                    <p className="text-lg font-semibold xl:text-xl">Entre em contato comigo:
+                    <div className="text-lg font-semibold xl:text-xl">Entre em contato comigo:
                         <section className="flex gap-8 my-2" id="contact">
                             <a href="https://www.linkedin.com/in/gabrieltoscanoml/" target="_blank" className="flex w-28">Linkedin <Image src={ linkedinSvg } alt="Linkedin icon" /></a>
                             <a href="https://github.com/gabrieltoscanoml" target="_blank" className="flex w-28">Github <Image src={ githubSvg } alt="Github icon" /></a>
                         </section>
                         Ou por email: gabrieltoscano1@outlook.com
-                    </p>
+                    </div>
                 </section>
                 <section className="lg:max-w-lg xl:max-w-2xl"> <Image src={ webImage } alt="Web Development image" max-width="100%"/></section>
             </main>
