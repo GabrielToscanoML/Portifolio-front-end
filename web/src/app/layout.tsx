efcf892d99b6109accf2bf33a65625837ec0f5d6
Gabriel <gabrieltoscano1@outlook.com>
2024-04-01 11:40:39 -0300

    alterando o idioma do metadata

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${roboto.variable} ${kanit.variable} font-sans`}>{children}</body>
     </html>
   )
