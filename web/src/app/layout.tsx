897407e6322627dd4e1eaf98bdc932a745633ce7
Gabriel <gabrieltoscano1@outlook.com>
2024-04-01 11:39:22 -0300

    Removendo um link do meu html no arquivo de layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={`${roboto.variable} ${kanit.variable} font-sans`}>{children}</body>
     </html>
   )
