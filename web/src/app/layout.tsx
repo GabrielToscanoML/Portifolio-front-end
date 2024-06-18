6e9f66b3fb07c6d77e2aeee3c256030684ce3365
Gabriel <gabrieltoscano1@outlook.com>
2024-06-18 07:01:50 -0300

    Adicionando suavidade ao scroll dos links de navegação

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className='scroll-smooth'>
       <body className={`${roboto.variable} ${kanit.variable} font-sans`}>{children}</body>
     </html>
   )
