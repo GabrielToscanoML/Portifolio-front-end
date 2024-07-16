28a49903c284b243e1a88b2c876f6b1ea6c7f58f
Gabriel Toscano <gabrieltoscano1@outlook.com>
2024-07-16 17:55:50 -0300

    Testando o redirecionamento

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,62 @@
+"use client"
+
 import { AboutMe } from "@/components/AboutMe";
 import { Header } from "@/components/Header";
 import { NavBar } from "@/components/NavBar";
 import { ProjectsSession } from "@/components/ProjectsSession";
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',  
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: '200px',
+//   },
+// };
 
 export default function Home() {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  useEffect(() => {
+    openModal();
+  }, [])
+
   return (
-    <>
-    <div className="sticky top-0 bg-white">
-      <NavBar />
-      <hr />
-    </div>
+    <>  
+      <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          // style={customStyles}
+          contentLabel="Example Modal"
+          className="flex mx-auto mt-48 justify-center items-center w-64 h-32"
+        >
+          <a 
+            href="https://toscano.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-black text-white p-4 rounded"
+            >
+              Abrir no navegador
+            </a>
+      </Modal>
+      <div className="sticky top-0 bg-white">
+        <NavBar />
+        <hr />
+      </div>
       <Header />
       <AboutMe />
       <hr />
