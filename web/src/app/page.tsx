0bb4e7ebf9aeec77d01266aa90c817ff5b1efbea
Gabriel Toscano <gabrieltoscano1@outlook.com>
2024-07-17 12:02:27 -0300

    Removendo o react modal e concluindo o redirecionamento

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,14 +5,9 @@ import { Header } from "@/components/Header";
 import { NavBar } from "@/components/NavBar";
 import { ProjectsSession } from "@/components/ProjectsSession";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-// import Modal from "react-modal";
-
-
-// import webImage from "../public/web-development.png";
+import { useEffect } from "react";
 
 export default function Home() {
-  const [modalIsOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -25,29 +20,8 @@ export default function Home() {
     }
   }, []);
 
-  function closeModal() {
-    setIsOpen(false);
-  }
-
   return (
     <>  
-      {/* <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          contentLabel="Example Modal"
-          className="flex mx-auto mt-48 justify-center items-center w-64 h-32"
-        >
-          <a 
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-            download
-            onClick={() => router.push("/")}
-            className="bg-red-400 text-white p-4 rounded"
-            >
-              Abrir no navegador
-          </a>
-      </Modal> */}
       <div className="sticky top-0 bg-white">
         <NavBar />
         <hr />
