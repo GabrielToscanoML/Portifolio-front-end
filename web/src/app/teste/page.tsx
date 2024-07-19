642a47f97eeebe7967f906615281466da6d46ed5
Gabriel Toscano <gabrieltoscano1@outlook.com>
2024-07-19 13:31:06 -0300

    testando dnv

--- web/src/app/teste/page.tsx
+++ web/src/app/teste/page.tsx
@@ -1,7 +1,29 @@
 "use client";
 
+import { AboutMe } from "@/components/AboutMe";
+import { Header } from "@/components/Header";
+import { NavBar } from "@/components/NavBar";
+import { ProjectsSession } from "@/components/ProjectsSession";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Teste() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (navigator.userAgent.match(/Instagram/i)) {
+      const currentUrl = window.location.href;
+      const link = document.createElement('a');
+        link.href = '/';
+        link.download = '';
+        link.click();
+        router.push(currentUrl);
+    }
+  }, []);
+
   return (
-    <h1>Olá!</h1>
+    <>  
+      <h1>OLÁAAAAAAAAAA</h1>
+    </>
   );
 }
