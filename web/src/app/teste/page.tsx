15f4e26205c026ac9b46c70712dd99522308e9a2
Gabriel Toscano <gabrieltoscano1@outlook.com>
2024-07-19 13:46:52 -0300

    testando dnv

--- web/src/app/teste/page.tsx
+++ web/src/app/teste/page.tsx
@@ -1,26 +1,6 @@
 "use client";
 
-import { AboutMe } from "@/components/AboutMe";
-import { Header } from "@/components/Header";
-import { NavBar } from "@/components/NavBar";
-import { ProjectsSession } from "@/components/ProjectsSession";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Teste() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (navigator.userAgent.match(/Instagram/i)) {
-      const currentUrl = window.location.pathname;
-      const link = document.createElement('a');
-        link.href = `https://toscano.vercel.app${currentUrl}`;
-        link.download = '';
-        link.click();
-        router.push(currentUrl);
-    }
-  }, []);
-
   return (
     <>  
       <h1>OLÁAAAAAAAAAA</h1>
