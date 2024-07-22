486528f1538dca79db265def2080b1f9a8e96f75
Gabriel Toscano <gabrieltoscano1@outlook.com>
2024-07-22 09:18:38 -0300

    Atualizando o link

--- web/src/components/InstagramRedirect.tsx
+++ web/src/components/InstagramRedirect.tsx
@@ -9,7 +9,7 @@ export default function InstagramRedirect() {
   useEffect(() => {
     if (navigator.userAgent.match(/Instagram/i)) {
       const currentUrl = window.location.pathname;
-      window.location.href = `https://sofoto.com.br${currentUrl}`;
+      window.location.href = `https://toscano.vercel.app/${currentUrl}`;
     }
   }, []);
 
