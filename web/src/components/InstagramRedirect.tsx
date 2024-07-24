ac97355b94c42fea3b8eb24b1ff36a63e05f4228
Gabriel <gabrieltoscano1@outlook.com>
2024-07-24 09:05:50 -0300

    Aplicando a logica nos dois sistemas

--- web/src/components/InstagramRedirect.tsx
+++ web/src/components/InstagramRedirect.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function InstagramRedirect() {
-  const router = useRouter();
-
+  // const router = useRouter();
   useEffect(() => {
-    if (navigator.userAgent.match(/Instagram/i)) {
-      const currentUrl = `https://toscano.vercel.app${window.location.pathname}`;
-      const link = document.createElement('a');
-        link.href = `x-web-search://?${link}`;
+    if (navigator.userAgent.match(/Instagram/i) || navigator.userAgent.match(/Gmail/i)) {
+      if (navigator.userAgent.match(/iPad|iPhone|iPod/i)) {
+        const currentUrl = `https://toscano.vercel.app${window.location.pathname}`;
+        // Se for um dispositivo iOS
+        const link = document.createElement('a');
+        link.href = `x-web-search://?${currentUrl}`;
         link.download = '';
         link.click();
-        router.push(currentUrl);
+      } else {
+        // Para outros dispositivos
+        const currentPath = window.location.pathname;
+        const link = document.createElement('a');
+        link.href =  `https://toscano.vercel.app${currentPath}`;
+        link.download = '';
+        link.click();
+        // router.push(currentUrl);
+      }
     }
   }, []);
 
-  return null;
 }
